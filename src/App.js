9090eba96009b860826b75a6df3eb6a986c4f7d8
xudayfi <xudayfiibraahim8@gmail.com>
2024-03-27 04:48:57 +0300

    fetching data from rest api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const MoviesList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/movies_api');
+        const data = await response.json();
+        setMovies(data);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className=' text-red-400 font-mono'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold mb-6">Movies List</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {movies.map((movie, index) => (
+          <div key={index} className="bg-white shadow-md rounded-lg overflow-hidden">
+            <img src={`http://127.0.0.1:8000${movie.Poster}`} alt={movie.Title} className="w-full h-64 object-cover" />
+
+            <div className="p-4">
+              <h2 className="text-lg font-bold">{movie.Title}</h2>
+              <p>Type: {movie.Type}</p>
+              <p>Year: {movie.Year}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default MoviesList;
