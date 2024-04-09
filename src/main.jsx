a81644dcfebf3f5d36251ddd0ff5cc36987354a2
xudayfi <xudayfiibraahim8@gmail.com>
2024-04-09 08:41:44 +0300

    finished todo app

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import Todo from "./todo";
+
+
+const Main = () => {
+    return (
+        <div>
+            <Todo />
+        </div>
+    )
+}
+
+
+export default Main;
--- src/todo.jsx
+++ src/todo.jsx
@@ -10,24 +10,48 @@ const Todo = () => {
   }
 
   const addNewTask = () => {
-    const newTodo = [...todoList, newTask]
-    setTodoList(newTodo)
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id))
+  }
+
+  const taskCompleted = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        console.log('is working');
+        if (task.id === id) {
+          return{...task, completed: true}
+          
+        }
+        else {
+          return task;
+        }
+      })
+    )
   }
 
   return (
-    <div className="ml-[36em] mt-[10em]">
-      {/*add task*/}
+    <div className="ml-[42em] mt-[15em]">
 
       <div>
         <input onChange={handleInputChanges} type="text" className="bg-indigo-200 rounded-lg p-4"/>
         <button onClick={addNewTask} type="submit" className="bg-indigo-400 p-3 rounded-lg ml-2 text-white">add</button>
       </div>
       {/*List*/}
-      <div className="mt-6">
+      <div className="mt-6" >
           {todoList.map((task) => {
             return <div className="flex mb-2">
-              <h1 className="pt-2 rounded w-[20em] text-1xl">{task}</h1>
-              <button className="bg-indigo-400 p-2 rounded-lg ml-2 text-white">X</button>
+              <h1 className="pt-2 rounded w-[20em] font-bold text-1xl" style={{color: task.completed ? "green" : "black"}}>{task.taskName}</h1>
+      
+              <button onClick={() => taskCompleted(task.id)} className="bg-indigo-400 p-2 rounded-lg ml-2 text-white">Completed</button>
+              <button onClick={() => deleteTask(task.id)} className="bg-indigo-400 p-2 rounded-lg ml-2 text-white">Delete</button>
             </div>
           })}
           
